94df5372b0fb209d2b1e87e56d0fad4aaa66d86f
Hamish Macpherson <hamstu@gmail.com>
2019-02-20 01:04:18 -0800

    perms on ssh key

--- .github/actions/build-and-deploy/entrypoint.sh
+++ .github/actions/build-and-deploy/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
+
+
 # Install node deps
 npm install
 npm install -g gatsby-cli
@@ -9,6 +11,7 @@ gatsby build
 
 # Put the SSH key in a temp file
 echo "${BTC_SERVER_PRIVATE_KEY}" > ./btc_id_rsa
+chmod 400 ./btc_id_rsa
 
 # Deploy to production
 rsync -avz -e "ssh -i ./btc_id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./public github@104.238.153.82:/var/www/betweentwocommits.com
