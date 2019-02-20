402be53f51c7f124279af0228d2ff5eb3eae86c6
Hamish Macpherson <hamstu@gmail.com>
2019-02-20 01:08:08 -0800

    add comments in deploy script

--- .github/actions/build-and-deploy/entrypoint.sh
+++ .github/actions/build-and-deploy/entrypoint.sh
@@ -1,13 +1,31 @@
 #!/bin/sh -l
 
+set -eu
 
+echo
+echo "======================================="
+echo "Installing Node deps"
+echo "======================================="
+echo
 
 # Install node deps
 npm install
 npm install -g gatsby-cli
 
+echo
+echo "======================================="
+echo "Building Gatsby site"
+echo "======================================="
+echo
+
 # Build the blog
 gatsby build
+
+echo
+echo "======================================="
+echo "Deploying to server"
+echo "======================================="
+echo
 
 # Put the SSH key in a temp file
 echo "${BTC_SERVER_PRIVATE_KEY}" > ./btc_id_rsa
