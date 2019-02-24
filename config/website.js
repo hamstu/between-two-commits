d9b663046836916d6e0788317388fb78b550fdb3
Brandon Wong <wong.brandon@rcgt.com>
2019-02-24 00:08:53 -0500

    updated website info

--- config/website.js
+++ config/website.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteTitle: 'Between Two Commits', // Navigation and Site Title
+  siteTitleAlt: 'What programmers do in their free time', // Alternative Site title for SEO
+  siteTitleShort: 'B2C', // short_name for manifest
+  siteUrl: 'https://betweentwocommits.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
-  author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  siteDescription: 'What programmers do in their free time',
+  author: 'Hamish Macpherson and Brandon Wong', // Author for schemaORGJSONLD
+  organization: 'b2c',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
-  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  userTwitter: '@between2commits', // Twitter Username
+  ogSiteName: '', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
 
@@ -21,8 +21,8 @@ module.exports = {
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
+  twitter: 'https://twitter.com/between2commits/',
+  twitterHandle: '@between2commits',
+  github: 'https://github.com/hamstu/',
   linkedin: '',
 }
