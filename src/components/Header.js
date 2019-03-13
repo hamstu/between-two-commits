fb273de7cf8cc927a09dfb4dcc1ab393460c607c
Hamish Macpherson <hamstu@gmail.com>
2019-03-12 22:47:06 -0700

    update package versions and fix warnings

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,66 +1,71 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import { css } from '@emotion/core'
 import theme from '../../config/theme'
 
 import Container from './Container'
 
-const Header = ({
-  dark,
-  bgColor = 'none',
-  siteTitle,
-  headerColor = 'black',
-}) => (
-  <header
-    css={css`
-      width: 100%;
-      flex-shrink: 0;
-      background: none;
-      padding: 30px 0 0 0;
-      background: ${dark ? '#090909' : `${bgColor}` || 'none'};
-    `}
-  >
-    <Container noVerticalPadding>
-      <nav
-        css={css`
-          width: 100%;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          color: ${headerColor};
-          a {
-            color: ${headerColor ? headerColor : theme.colors.body_color};
-          }
-          a:hover {
-            color: ${headerColor === theme.colors.white
-              ? 'white'
-              : theme.colors.link_color_hover};
-          }
-        `}
-      >
-        <Link to="/" aria-label="go to homepage" activeClassName="active">
-          {siteTitle}
-        </Link>
-        <div
+const Header = ({ dark, bgColor = 'none', headerColor = 'black' }) => {
+  const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  return (
+    <header
+      css={css`
+        width: 100%;
+        flex-shrink: 0;
+        background: none;
+        padding: 30px 0 0 0;
+        background: ${dark ? '#090909' : `${bgColor}` || 'none'};
+      `}
+    >
+      <Container noVerticalPadding>
+        <nav
           css={css`
-            font-size: 16px;
-            line-height: 1.25;
+            width: 100%;
             display: flex;
+            justify-content: space-between;
             align-items: center;
+            color: ${headerColor};
             a {
-              color: ${dark ? '#fbfbfb' : 'rgba(0,0,0,0.85)'};
-              text-decoration: none;
-              & + a {
-                margin-left: 32px;
-              }
+              color: ${headerColor ? headerColor : theme.colors.body_color};
             }
-            .active {
-              display: none;
-              visibility: hidden;
+            a:hover {
+              color: ${headerColor === theme.colors.white
+                ? 'white'
+                : theme.colors.link_color_hover};
             }
           `}
         >
-          {/*
+          <Link to="/" aria-label="go to homepage" activeClassName="active">
+            {data.site.siteMetadata.title}
+          </Link>
+          <div
+            css={css`
+              font-size: 16px;
+              line-height: 1.25;
+              display: flex;
+              align-items: center;
+              a {
+                color: ${dark ? '#fbfbfb' : 'rgba(0,0,0,0.85)'};
+                text-decoration: none;
+                & + a {
+                  margin-left: 32px;
+                }
+              }
+              .active {
+                display: none;
+                visibility: hidden;
+              }
+            `}
+          >
+            {/*
           <Link
             to="/blog"
             activeClassName="active"
@@ -69,20 +74,11 @@ const Header = ({
             Blog
           </Link>
           */}
-        </div>
-      </nav>
-    </Container>
-  </header>
-)
+          </div>
+        </nav>
+      </Container>
+    </header>
+  )
+}
 
 export default Header
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
